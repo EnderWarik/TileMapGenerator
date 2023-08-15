483379dc43c79a570e3df86c0049c6df3f44b08e
EnderWarik <2020102437@pnu.edu.ru>
2023-08-15 18:38:48 +1000

    init

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "1.8.21"
+}
+
+group = "madproject.ew"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(11)
+}
